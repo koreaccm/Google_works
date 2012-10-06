52eb22ed0cf5d666eb7b582c61a3caefcc7d94a4
koreaccm <koreaccm@gmail.com>
2012-10-07 00:30:15 +0900

    file update

--- excel_test.py
+++ excel_test.py
@@ -1,3 +1,4 @@
+# example 1
 from xlrd import open_workbook
 wb = open_workbook('test.xls')
 for s in wb.sheets():
@@ -8,3 +9,62 @@ for s in wb.sheets():
             values.append(s.cell(row,col).value)
         print ','.join(values)
     print
+    
+
+# example 2
+from xlrd import cellname
+
+book = open_workbook('test.xls')
+sheet = book.sheet_by_index(0)
+print sheet.name
+print sheet.nrows
+print sheet.ncols
+
+for row_index in range(sheet.nrows):
+    for col_index in range(sheet.ncols):
+        print cellname(row_index, col_index), '-',
+        print sheet.cell(row_index, col_index).value
+    
+#example 3
+from xlrd import XL_CELL_TEXT
+
+book = open_workbook('test.xls')
+sheet = book.sheet_by_index(0)
+
+cell = sheet.cell(0, 0)
+print cell
+print cell.value
+print cell.ctype==XL_CELL_TEXT
+
+for i in range(sheet.ncols):
+    print sheet.cell_type(0,i), sheet.cell_value(0,i)
+    
+#example 4
+book = open_workbook('test.xls')
+sheet0 = book.sheet_by_index(0)
+
+print sheet0.row(0)
+print sheet0.col(0)
+print
+print sheet0.row_slice(0,1)
+print sheet0.row_slice(0,1,2)
+print sheet0.row_values(0,1)
+print sheet0.row_values(0,1,2)
+print sheet0.row_types(0,1)
+print sheet0.row_types(0,1,2)
+print
+print sheet0.col_slice(0,1)
+print sheet0.col_slice(0,1,2)
+print sheet0.col_values(0,1)
+print sheet0.col_values(0,1,2)
+print sheet0.col_types(0,1)
+print sheet0.col_types(0,1,2)
+
+
+
+
+
+
+
+
+    
