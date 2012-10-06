aaaac28bf16563197ea5536837e0da49324d2f50
koreaccm <koreaccm@gmail.com>
2012-10-06 21:34:38 +0900

    update - openanything.py - profile_inspection.py

---
+++ profile_inspection.py
@@ -0,0 +1,18 @@
+#-*- encoding: cp949 -*-
+import httplib, urllib, urllib2
+import StringIO, gzip
+
+httplib.HTTPConnection.debuglevel = 1
+query_args = {'query':'house'}
+encoded_args = 'search.naver?'+ urllib.urlencode(query_args)
+print encoded_args
+
+request = urllib2.Request('http://searchc.naver.com/')
+request.add_header('User-agent', 'Mozilla/5.0')
+request.add_header('Accept-encoding', 'gzip')
+response = urllib2.urlopen(request, encoded_args)
+
+compressedstream = StringIO.StringIO(response.read())
+gzipper = gzip.GzipFile(fileobj=compressedstream)
+
+print gzipper.read()
