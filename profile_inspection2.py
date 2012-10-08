e68018e864d7ffec2a7bfe9195740f73127c81ed
koreaccm <koreaccm@gmail.com>
2012-10-08 16:47:20 +0900

    profile~.py works well

--- profile_inspection2.py
+++ profile_inspection2.py
@@ -8,22 +8,21 @@ sheet0 = xls.sheet_by_index(0)
 for row_index in range(sheet0.nrows):
 
     keyword = sheet0.cell(row_index,0).value
-    query_args = {'query':keyword,'where':'ls'}
-    encoded_args = 'search.naver?'+ urllib.urlencode(query_args)
-    print encoded_args
-
-    request = urllib2.Request('http://search.naver.com/')
+    params = {'query':keyword}
+    enc_params = urllib.urlencode(params)
+    
+    request = urllib2.Request('http://search.naver.com/'+'search.naver'+'?'+enc_params)
     request.add_header('User-agent', 'Mozilla/5.0')
     request.add_header('Accept-encoding', 'gzip')
-    response = urllib2.urlopen(request, encoded_args)
+    response = urllib2.urlopen(request)
 
     compressedstream = StringIO.StringIO(response.read())
     gzipper = gzip.GzipFile(fileobj=compressedstream)
 
-    data = gzipper.read().encode('utf-8')
+    data = gzipper.read(100000).encode('utf-8')
     print data
     
-    if (data.find('section') != -1 ):
+    if (data.find('people_info section') != -1 ):
         print 'yes'
     else:
         print 'no'
